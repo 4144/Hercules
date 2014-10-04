e772786ebfb1b851a06960fa0443f86998185ba2
Andrei Karas <akaras@inbox.ru>
2014-10-04 17:17:24 +0300

    Add import tables script.

---
+++ sql-files/import.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CMD=mysql -u root -p hercules
+
+$CMD <main.sql
+$CMD <item_db.sql
+$CMD <item_db2.sql
+$CMD <mob_db.sql
+$CMD <mob_db2.sql
+$CMD <mob_skill_db.sql
+$CMD <mob_skill_db2.sql
+$CMD <logs.sql
+$CMD <item_db_re.sql
+$CMD <mob_db_re.sql
+$CMD <mob_skill_db_re.sql
