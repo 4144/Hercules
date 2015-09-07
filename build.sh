156c0508b4f7bc4c9588bc20d85e14f799833893
Andrei Karas <akaras@inbox.ru>
2015-09-07 23:02:21 +0300

    In build script enable full sanity checks.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 autoreconf
-./configure --enable-sanitize --disable-lto --enable-packetver=20150000
+./configure --enable-sanitize=full --disable-lto --enable-packetver=20150000
 #./configure --enable-static --disable-lto --enable-packetver=20150000
 make
